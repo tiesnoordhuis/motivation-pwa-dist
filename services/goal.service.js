d00cf01276d8b9bef96612e9c6fba81b76e6a075
Ties Noordhuis <tiesnoordhuis@gmail.com>
2026-01-28 22:18:17 +0100

    Updates

--- services/goal.service.js
+++ services/goal.service.js
@@ -1,8 +1,7 @@
-// Toggle this manual flag for production build until we have environment variables
-const IS_PROD = false;
-const API_URL = IS_PROD
-    ? 'http://192.168.178.170:3000'
-    : 'http://localhost:3000';
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const API_URL = isLocal
+    ? 'http://localhost:3000'
+    : 'http://192.168.178.170:3000';
 export class GoalService {
     static async fetchGoals() {
         const response = await fetch(`${API_URL}/api/goals`);
